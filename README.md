72cd5490ecdabe8a442d1592b0c376752a962b4a
Niklles <max777944@inbox.ru>
2025-07-10 03:18:30 +0300

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Домашнее задание к занятию "Работа с HTML-формами"
+
+[![Build status]()](https://ci.appveyor.com/project/Niklles/ahj-form)
+
---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+import Popover from "./Popover";
+
+const popoverFactory = new Popover();
+let actualPopovers = [];
+const popoverText =
+  "And here's some amazing content. It's very engaging. Right?";
+
+const container = document.querySelector(".container");
+
+// eslint-disable-next-line no-unused-vars
+const showPopover = (headerText, bodyText, el) => {
+  actualPopovers.push({
+    name: el.name,
+    id: popoverFactory.show(headerText, bodyText, el),
+  });
+};
+
+const onClick = (e) => {
+  const { target } = e;
+
+  if (target.classList.contains("btn")) {
+    const matchingPopover = actualPopovers.find(
+      (popover) => popover.name === target.name,
+    );
+
+    if (matchingPopover) {
+      // Поповер уже существует, поэтому закрываем его
+      popoverFactory.remove(matchingPopover.id);
+      actualPopovers = actualPopovers.filter(
+        (popover) => popover.id !== matchingPopover.id,
+      );
+    } else {
+      // Поповер еще не существует, поэтому создаем его
+      actualPopovers.push({
+        name: target.name,
+        id: popoverFactory.show("Popover title", popoverText, target),
+      });
+    }
+  }
+};
+
+window.addEventListener("click", onClick);
+
+const button = document.createElement("button");
+button.type = "button";
+button.innerHTML = "Click to toggle popover";
+button.classList.add("btn", "btn-lg", "btn-danger");
+
+const tasks = container.querySelectorAll(".task");
+const task1 = tasks[0];
+task1.appendChild(button);
